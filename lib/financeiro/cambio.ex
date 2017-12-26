587593ccff4b2af345639049af00be557f07e248
joaothallis <joaothallis@id.uff.br>
2017-12-26 16:58:01 -0200

    Feat: Peso das moedas

---
+++ lib/financeiro/cambio.ex
@@ -0,0 +1,15 @@
+defmodule Cambio do
+    @moduledoc """
+    Realiza câmbio de moedas
+    """
+  
+    @doc """
+    """
+    def troca(usuarios, usuario) do
+      
+    end
+
+    def taxa(primeira_moeda, segunda_moeda) do
+        
+    end
+  end
