a9801ac23923132006fca19fbe8325654aa54ef1
joaothallis <joaothallis@id.uff.br>
2018-01-01 11:38:15 -0200

    Feat: Módulo sair

---
+++ lib/financeiro/critico.ex
@@ -0,0 +1,29 @@
+
+defmodule Critico do
+  @moduledoc """
+  Interrompe a execução do programa em caso de erros críticos.
+  """
+
+  @doc """
+  Interrompe a execução do programa caso o usuário não exista.
+
+  """
+  def usuario_falha(usuarios, usuario) do
+    if Keyword.fetch(usuarios, usuario) == :error do
+      IO.puts "Usuário não existe"
+      System.stop(0)  
+    end
+  end
+
+  @doc """
+  Interrompe a execução do programa caso o dinheiro de uma transação não exista.
+
+  """
+  def dinheiro_falha(quantia) do
+    unless quantia > 0 do
+      IO.puts "Dinheiro insuficiente."
+      System.stop(0) 
+    end
+  
+  end
+end
