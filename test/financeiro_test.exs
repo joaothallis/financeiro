eca7e4baf9f266415d93d1b83143812a197a65f5
João Thallis <joaothallis@id.uff.br>
2018-01-06 22:04:34 -0200

    Test: Remove teste duplicado

--- test/financeiro_test.exs
+++ test/financeiro_test.exs
@@ -2,15 +2,6 @@ defmodule FinanceiroTest do
   use ExUnit.Case
   doctest Financeiro
 
-  test "verifica se usuário existe" do
-    usuarios = [
-      john: 0,
-      stone: 0,
-      maria: 0
-    ]
-    assert Keyword.fetch(usuarios, :john) == {:ok, 0}
-  end
-
   test "remove espaço vazio e transforma em atom" do
     assert Financeiro.string_atom("john \n") == :john
   end
